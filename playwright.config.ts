5179601921ec93d05b838a279b70cc51fbb69e0c
Priyanka Vaka <Priyanka.Vaka@gmail.com>
2026-09-14 12:26:13 +0000

    commiting the chages

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@playwright/test';
+import { envConfig } from './src/config/environment';
+
+export default defineConfig({
+  testDir: './src/tests',
+
+  use: {
+    baseURL: envConfig.baseUrl,
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+  },
+
+  workers: 4,
+
+  retries: process.env.CI ? 2 : 0,
+
+  reporter: [['html', { open: 'never' }]],
+});
---
+++ src/pages/LoginPage.ts
@@ -0,0 +1,22 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+    readonly page: Page;
+    readonly username: Locator;
+    readonly password: Locator;
+    readonly loginButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+
+        this.username = page.getByLabel('Username');
+        this.password = page.getByLabel('Password');
+        this.loginButton = page.getByRole('button', { name: 'Login' });
+    }
+
+    async login(username: string, password: string) {
+        await this.username.fill(username);
+        await this.password.fill(password);
+        await this.loginButton.click();
+    }
+}
