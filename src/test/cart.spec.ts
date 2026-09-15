c4a0df28a8efed59efdff842bb9144e74352caa3
Priyanka Vaka <priyanka.vaka@gmail.com>
2026-09-15 17:56:37 +0530

    modifying the page

--- src/test/cart.spec.ts
+++ src/test/cart.spec.ts
@@ -1,22 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/loginpage';
 
 test('Add product to cart', async ({ page }) => {
 
-  // Open application
   await page.goto('/');
 
-  // Login
-  await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
-  await page.getByRole('button', { name: 'Login' }).click();
+  const loginPage = new LoginPage(page);
 
-  // Verify Products page
+  await loginPage.login('standard_user', 'secret_sauce');
+
   await expect(page.getByText('Products')).toBeVisible();
 
-  // Add Sauce Labs Backpack to cart
   await page.getByRole('button', { name: 'Add to cart' }).first().click();
 
-  // Verify cart contains 1 item
   await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
-
 });
