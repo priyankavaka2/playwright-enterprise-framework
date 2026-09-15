8f6a4925ac3cae593092af931760f3fe97f94fca
Priyanka Vaka <priyanka.vaka@gmail.com>
2026-09-15 16:21:59 +0530

    changed config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,12 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './src/test',
 
-  use: {
-    channel: 'chrome',
-    headless: false,
-    baseURL: 'https://www.saucedemo.com',
-    screenshot: 'only-on-failure',
-    trace: 'on',
+ use: {
+  channel: 'chrome',
+  headless: !!process.env.CI,
+  baseURL: 'https://www.saucedemo.com',
+  screenshot: 'only-on-failure',
+  trace: 'on',
   },
 
   projects: [
